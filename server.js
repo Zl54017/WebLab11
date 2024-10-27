6648e1a43e46aa78354e26f9f0740e5576f98ffb
Zvonko Lelas <148819650+Zl54017@users.noreply.github.com>
2024-10-27 13:26:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -31,15 +31,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3000;
-if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
-  config.baseURL = `http://localhost:${port}`;
-}
 
 const config = {
   authRequired: false,
   auth0Logout: true,
   baseURL: process.env.BASE_URL || `http://localhost:${port}`
 };
+
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
 
 app.use(auth(config));
 
